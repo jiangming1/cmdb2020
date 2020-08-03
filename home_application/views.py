0a66fd631b5fc1dc919cd7baab7468062959f325
Your Name <2100803@qq.com>
2020-08-03 17:32:25 +0800

    a

--- home_application/views.py
+++ home_application/views.py
@@ -7,7 +7,7 @@ import base64
 import datetime
 import json
 import time
-from celery.tast imporrt task
+from celery.task imporrt task
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
